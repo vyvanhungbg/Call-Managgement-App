87593797568077d1257f903c91fddb3f9e2e894a
vyhung <vyvanhung2882001bg@gmail.com>
2022-03-12 10:20:20 +0700

    create layout call statistic - custom tablayout sucess

--- app/src/main/java/com/franky/callmanagement/fragments/CallStatisticsFragment.java
+++ app/src/main/java/com/franky/callmanagement/fragments/CallStatisticsFragment.java
@@ -1,5 +1,7 @@
 package com.franky.callmanagement.fragments;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.franky.callmanagement.R;
 
@@ -16,8 +19,13 @@ import com.franky.callmanagement.R;
  * Use the {@link CallStatisticsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CallStatisticsFragment extends Fragment  {
+public class CallStatisticsFragment extends Fragment implements View.OnClickListener {
 
+    ColorStateList def;
+    TextView item1;
+    TextView item2;
+    TextView item3;
+    TextView select;
 
     public static CallStatisticsFragment newInstance() {
         CallStatisticsFragment fragment = new CallStatisticsFragment();
@@ -40,11 +48,39 @@ public class CallStatisticsFragment extends Fragment  {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        View view = inflater.inflate(R.layout.fragment_call_statistics, container, false);
+        item1 = view.findViewById(R.id.item1);
+        item2 = view.findViewById(R.id.item2);
+        item3 = view.findViewById(R.id.item3);
+        item1.setOnClickListener(this);
+        item2.setOnClickListener(this);
+        item3.setOnClickListener(this);
+        select = view.findViewById(R.id.select);
+        def = item2.getTextColors();
 
-
-        return inflater.inflate(R.layout.fragment_call_statistics, container, false);
+        return view;
     }
 
-
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.item1){
+            select.animate().x(0).setDuration(200);
+            item1.setTextColor(Color.WHITE);
+            item2.setTextColor(def);
+            item3.setTextColor(def);
+        } else if (view.getId() == R.id.item2){
+            item1.setTextColor(def);
+            item2.setTextColor(Color.WHITE);
+            item3.setTextColor(def);
+            int size = item2.getWidth();
+            select.animate().x(size).setDuration(200);
+        } else if (view.getId() == R.id.item3){
+            item1.setTextColor(def);
+            item3.setTextColor(Color.WHITE);
+            item2.setTextColor(def);
+            int size = item2.getWidth() * 2;
+            select.animate().x(size).setDuration(200);
+        }
+    }
 
 }
